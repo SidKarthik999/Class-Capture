810a933b0cc233bde787ed4f458201b400591ace
SidKarthik999 <79047961+SidKarthik999@users.noreply.github.com>
2023-02-26 11:45:17 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import flask
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/downloaded') # this is a job for GET, not POST
+def download_zip():
+    return send_file(
+        '/Users/siddanthkarthik/PycharmProjects/ClassCapture/ClassCapture_Master.zip',
+        mimetype='application/zip',
+        as_attachment=True
+    )
